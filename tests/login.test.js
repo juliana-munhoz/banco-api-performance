502ed24ceddca8766c462fa0f52f73db6fede5ce
juliana-munhoz <julianamunhoz@outlook.com>
2026-04-16 12:02:04 -0300

    Adicionado arquivo de configuração e variaveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+import { obterBaseURL } from '../utils/variaveis.js';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
@@ -18,7 +19,7 @@ export const options = {
 };
 
 export default function () {
-  const url = 'http://localhost:3000/login';
+  const url = obterBaseURL() + '/login';
 
   const payload = JSON.stringify(postLogin);
 
